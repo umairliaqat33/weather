fe1b38a483fcca81064ab9405fe2bb2908edb6d3
Umair <umairliaqat552@gmail.com>
2022-08-31 14:43:44 +0500

    Changed app icon

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ import 'package:weather/screens/home_screen.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
